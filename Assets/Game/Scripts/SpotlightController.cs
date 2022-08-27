eaa05398c8f613e89291e17103f5c9ff876ea492
Shubh Gawhade <51043720+shubhgawhade@users.noreply.github.com>
2022-08-27 20:49:39 +0530

    Added Navmesh to AI
    - Navmesh
    - Spotlight controller with battery and timer

---
+++ Assets/Game/Scripts/SpotlightController.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightController : MonoBehaviour
+{
+    public bool isOn;
+    public bool cooldown;
+
+    private Light spotlight;
+    public float battery;
+    public float dischargeRate;
+    public float rechargeRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spotlight = GetComponent<Light>();
+        isOn = spotlight.enabled;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !cooldown)
+        {
+            ToggleSpotlight();
+        }
+
+        BatteryDischarge();
+    }
+
+    IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        cooldown = false;
+    }
+
+    private void BatteryDischarge()
+    {
+        if (isOn)
+        {
+            if (battery - dischargeRate >= 0)
+            {
+                battery -= dischargeRate;
+            }
+            else
+            {
+                battery = 0;
+                cooldown = true;
+                StartCoroutine(Wait(1.5f));
+                ToggleSpotlight();
+            }
+        }
+        else if (!cooldown)
+        {
+            if (battery + rechargeRate <= 100)
+            {
+                battery += rechargeRate;
+            }
+            else
+            {
+                battery = 100;
+            }
+        }
+    }
+
+    private void ToggleSpotlight()
+    {
+        isOn = !isOn;
+        spotlight.enabled = isOn;
+    }
+}
